724819d5231ed1f337f34eadd67dd1e7176f2d2d
牧本夏美 <makimotonatsumi@nanase.local>
2024-11-29 02:31:49 +0900

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  add_flash_types :success, :danger, :info, :warning
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
 
   private
+
+  def require_login
+    return if logged_in?
+
+    redirect_to login_path
+  end
 
   def not_authenticated
     redirect_to login_path
