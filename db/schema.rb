35fd702438ff239a615661096a8de2f2bde323d0
牧本夏美 <makimotonatsumi@nanase.local>
2024-12-01 20:46:05 +0900

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_01_085039) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_01_080811) do
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.bigint "item_id", null: false
