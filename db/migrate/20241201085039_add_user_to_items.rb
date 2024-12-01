69218b306be8466183a22d0bbf947d3e92df8710
牧本夏美 <makimotonatsumi@nanase.local>
2024-12-01 19:24:04 +0900

    comment

--- db/migrate/20241201085039_add_user_to_items.rb
+++ db/migrate/20241201085039_add_user_to_items.rb
@@ -1,5 +1,6 @@
 class AddUserToItems < ActiveRecord::Migration[7.1]
   def change
-    add_reference :items, :user, null:, foreign_key: true
+    add_column :users, :user_id, :integer
+    add_reference :items, :user, null:, foreign_key: true, null: false
   end
 end
